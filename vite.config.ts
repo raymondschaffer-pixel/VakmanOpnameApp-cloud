f9770c8d9fc15edf2c04d7429165e2b9236f8add
raymondschaffer-pixel <raymondschaffer@gmail.com>
2026-02-05 20:36:11 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
@@ -16,14 +15,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage']
-        }
-      }
-    }
+    sourcemap: false
   }
 });
